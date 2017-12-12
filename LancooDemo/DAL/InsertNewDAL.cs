33a8f197b3d3137aded522a494243e41f53c08cc
Administrator <Administrator@X-PC>
2017-12-12 15:30:08 +0800

    修改了更新地表的SQL语句，增加了时间戳

--- LancooDemo/DAL/InsertNewDAL.cs
+++ LancooDemo/DAL/InsertNewDAL.cs
@@ -15,7 +15,7 @@ namespace LancooDemo.DAL
 
         public int InsertUesr_Use()
         {
-            string sqlStr = "insert into dbo.User_Use select UserID, ResourceID, count(ResourceID) as Times from dbo.ResourceUse"
+            string sqlStr = "insert into dbo.User_Use（UserID, ResourceID, Times, UpdateTime） select UserID, ResourceID, count(ResourceID) as Times, CONVERT(datetime,GETDATE()) from dbo.ResourceUse"
                              + " group by UserID, ResourceID having count(*) > 0 order by Times desc";
             List<IDataParameter> parameters = Param()
                 .Build();
@@ -26,7 +26,7 @@ namespace LancooDemo.DAL
 
         public int InsertSubject_Use()
         {
-            string sqlStr = "insert into dbo.Subject_Use select SubjectID, ResourceID, count(SubjectID) as Times from dbo.ResourceUse" 
+            string sqlStr = "insert into dbo.Subject_Use(SubjectID, ResourceID, Times, UpdateTime) select SubjectID, ResourceID, count(SubjectID) as Times, CONVERT(datetime,GETDATE()) from dbo.ResourceUse"
                             + " group by SubjectID, ResourceID having count(*) > 0 order by Times desc";
             List<IDataParameter> parameters = Param()
                 .Build();
@@ -37,7 +37,7 @@ namespace LancooDemo.DAL
 
         public int InsertResource_UseTotal()
         {
-            string sqlStr = "insert into dbo.Resource_UseTotal select ResourceID, count(ResourceID) as Times from dbo.ResourceUse"
+            string sqlStr = "insert into dbo.Resource_UseTotal(ResourceID, Times, UpdateTime) select ResourceID, count(ResourceID) as Times, CONVERT(datetime,GETDATE()) from dbo.ResourceUse"
                              + " group by ResourceID having count(*)>=0 order by Times desc";
             List<IDataParameter> parameters = Param()
                 .Build();
